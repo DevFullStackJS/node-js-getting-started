f1382f4ac6393ee77e569d306e0760ef5c480e34
devJSFullStack <dev.fullstack.js0311@gmail.com>
2020-07-20 12:50:46 +0300

    test Build

---
+++ build/precache-manifest.8f26ed9ba165ee14ff84167f976e67ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904dc2b1235f2e671a417d3946b73c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cbe8efefe2420d40b73",
+    "url": "/static/css/main.099a6a36.chunk.css"
+  },
+  {
+    "revision": "f8b8c55da76d9f09fa79",
+    "url": "/static/js/2.44e18bf5.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/static/js/2.44e18bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbe8efefe2420d40b73",
+    "url": "/static/js/main.280587aa.chunk.js"
+  },
+  {
+    "revision": "5397e7c9d35b6d0b122b",
+    "url": "/static/js/runtime-main.b9b05f81.js"
+  },
+  {
+    "revision": "7940e6ce3d1c814582d82554510dff6b",
+    "url": "/static/media/logo.7940e6ce.png"
+  }
+]);
